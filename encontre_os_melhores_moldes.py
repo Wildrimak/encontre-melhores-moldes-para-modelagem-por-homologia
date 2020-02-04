680dc56b5e960a2125ca7e762fd788141c34484c
Wildrimak <finalwildrimak@gmail.com>
2020-02-04 16:34:45 -0300

    Destrichando algoritmo da solução antes da implementação real

--- encontre_os_melhores_moldes.py
+++ encontre_os_melhores_moldes.py
@@ -77,6 +77,20 @@ def _calcula_score(sequencia_teorica, sequence_analisada):
 def _count(sequence):
 	return len([c for c in sequence])
 
+def calcula_melhores_sequencias(sequencia_original_str, lista_proteinas_homologas):
+	"""
+	
+	for proteina in lista_proteinas_homologas:
+		
+	proteina = [proteina.nome, proteina.score, proteina.lista_aminoacidos]	
+
+		for aminoacido in proteina.get_lista_aminoacidos():
+			aminoacido = [aminoacido.ordem, aminoacido.sigla, aminoacido.sigla_no_original, aminoacido.proteina, aminoacido.lista_proteinas_concorrentes]
+
+	"""
+	pass
+
+
 def sequencias_analisadas():
 
 	simula_sequencias = [
@@ -103,7 +117,7 @@ def sequencias_analisadas():
 		{"SRE21":"ABCDEFFBJGHLIJWKLVMNOZPQTRSNTUVWXYZ"},
 		{"SRE22":"ABBCDEAKFGHIOPQRLSTUVZFXYZSY"},
 		{"SRE23":"ABCDEFFBJGHLIJWKLVMNOZPQTRSNTUVWXYZ"},
-		{"SRE24":"ABBCDEAKFGHIOPQRLSTUVZFXYZSY"},
+		{"SRE24":"ABBCDEAKFGHIOPQRLSSUVZFXYZSY"},
 		{"SRE25":"ABCDEFFBJGHLIJWKLVMNOZPQTRSNTUVWXYZ"},
 		{"SRE26":"ABBCDEAKFGHIOPQRLSTUVZFXYZSY"}
 	] 
@@ -116,7 +130,7 @@ def sequencias_analisadas():
 		{"S5":"trsgacbdvxcvsgfPQbsfzxvXYZ"}
 	]
 
-	return nova_sequencia
+	return simula_sequencias
 
 
 def sequencia_teorica():
@@ -128,3 +142,37 @@ def sequencia_teorica():
 
 if __name__ == '__main__':
 	main()
+
+"""
+
+Simplificando algoritmo:
+
+
+Objetivo: Entregar menor quantidade de proteinas possiveis capaz de formar uma modelagem por homologia
+
+Passo 1: Ter a sequencia de aminoacidos original
+Passo 2: Ter a lista de sequencias homologas que podem formar uma modelagem por homologia
+Passo 3: Marcar a ordem de cada aminoacido na sequencia original por transforma-lo em uma lista de objetos aminoacidos 
+na seguinte forma: aminoacido.ordem, aminoacido.sigla
+Passo 4: Marcar a ordem de cada aminoacido em cada sequencia da lista
+Passo 5: Fazer a união resultante de todos os aminoacidos recebendo a sequencia original e a lista de sequencias
+	Sub-passo 5.1: Criar lista de aminoacidos resultantes vazia
+	Sub-passo 5.2: Criar is_igual_original = true
+	Sub-passo 5.3: Iterar sobre a lista de sequencias
+	Sub-passo 5.4: Iterar sobre a lista de aminoacidos (aminoacido.ordem, aminoacido.sigla) juntamente com lista de aminoacidos orginal
+	Sub-passo 5.5: Verificar se a lista de aminoacidos resultantes tem algum elemento na posição aminoacido.ordem - 1
+	Sub-passo 5.6: Se tiver faça acontecer nada
+	Sub-passo 5.7: Se não tiver verificar se o par de aminoacidos é igual:
+	Sub-passo 5.8: Se for igual adicionar aminoacido na lista de aminoacidos resultantes e fazer is_igual_original = true
+	Sub-passo 5.9: Se o par de aminoacidos não forem iguais (perguntar se nao tem ninguem naquela casa e se nao tiver) 
+	fazer o is_igual_original = false 	
+	Sub-passo 5.10 retornar tupla(lista de aminoacidos resultantes, is_igual_original)
+Passo 6: Devolver a tupla (sequencia_resultante, is_igual_original)
+Passo 7: Se is_igual_original for true fazer iniciar_interacao+=1 
+Passo X: Remover a primeira sequencia da minha lista de sequencias
+Passo X: Me chamar novamente passando (sequencia_original, lista_de_sequencias, iniciar_interacao) e esperar retorno
+Passo X: Se no retorno is_igual_original for falso adicionar a sequencia removida novamente e continuar algoritmo
+Passo X: Quando iniciar_interacao for maior que tamanho sequencia original parar a recursividade devolvendo a lista 
+-- Passo 8: Se is_igual_original for falso encerrar algoritmo entregando a lista de sequencias vinda com ele
+
+"""
